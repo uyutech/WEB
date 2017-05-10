c6df5d9406472e2cfb3923cee23edad5cd3586d0
张俊 <zj61865502@163.com>
2017-05-10 17:18:21 +0800

    修改globalexceptionhandler

--- app_zhuanquan/server/src/main/java/com/zhuanquan/app/server/controller/common/BaseController.java
+++ app_zhuanquan/server/src/main/java/com/zhuanquan/app/server/controller/common/BaseController.java
@@ -1,19 +1,9 @@
 package com.zhuanquan.app.server.controller.common;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
-import com.zhuanquan.app.common.exception.BizErrorCode;
-import com.zhuanquan.app.common.exception.BizException;
-import com.zhuanquan.app.common.view.ApiResponse;
 
 //基类，统一处理所有的异常
 public class BaseController {
